51226b6b363545ab413e8acd98fd16495d9045c8
zhang <377207104@qq.com>
2020-03-29 22:13:45 +0800

    测试Integer值传递

---
+++ src/main/java/com/zhang/springboot/thread/test/MyTest.java
@@ -0,0 +1,16 @@
+package com.zhang.springboot.thread.test;
+
+public class MyTest {
+  public void addInteger(Integer num){
+    num = num+1;
+    System.out.println(num);
+  }
+
+  public static void main(String[] args) {
+    MyTest myTest = new MyTest();
+    Integer num = new Integer(1);
+    myTest.addInteger(num);
+    System.out.println(num);
+  }
+
+}
